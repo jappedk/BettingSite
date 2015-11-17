15ccd6840891338d16caa43a386a482283ad2cff
jeppe <jappesg@gmail.com>
2015-11-17 19:39:50 +0100

    bla

---
+++ BettingClient/BettingClient/Dao/Dao.cs
@@ -0,0 +1,24 @@
+﻿using BettingClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BettingSite.Dao
+{
+    public static class Dao
+    {
+        private static BettingSiteEntities DB;
+
+        public static BettingSiteEntities getDB()
+        {
+            if (DB == null)
+            {
+                DB = new BettingSiteEntities();
+            }
+            return DB;
+        }
+
+    }
+}
---
+++ BettingClient/BettingClient/Models/Wrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BettingClient.Models
+{
+    public class Wrapper
+    {
+        public Bruger bruger { get; set; }
+        public wager onewager { get; set; }
+        public List<wager> wager { get; set; }
+        public bet emptybet { get; set; }
+
+    }
+}
